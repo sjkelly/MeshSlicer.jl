9a02c50d5554092d9a1d1733de39e6d47aa34c01
Steve Kelly <kd2cca@gmail.com>
2014-05-23 11:32:49 -0400

    add tests for rotation and line segements, also use test handlers

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,7 +41,12 @@ end
 
 # Test bounds on translated cube
 # translate([1,2,3])cube([4,5,6]);
-testBounds2 = Bounds(5.0,7.0,9.0,1.0,2.0,3.0)
+testBounds2 = Bounds(4.0,6.0,9.0,1.0,2.0,4.0)
+updateFace = Face(Array[[5.0,3.0,4.0],
+                      [2.0,7.0,4.0],
+                      [2.0,3.0,3.0]],
+                      [-1.0,0.0,0.0]) # contains correct bounds for cube
+update!(testBounds2, updateFace)
 resultmesh2 = PolygonMesh("./data/translated_cube.stl")
 Test.with_handler(test_handler) do
     @test resultmesh2.bounds.xmax == testBounds2.xmax
@@ -52,22 +57,59 @@ Test.with_handler(test_handler) do
     @test resultmesh2.bounds.zmin == testBounds2.zmin
     @test resultmesh2.bounds == testBounds2
 end
-println(resultmesh2)
-println(PolygonSlice(resultmesh2, 5.0))
 
 
 # Test getlinesegment
-testpoints = Array[[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,1.0,1.0]]
+testpoint1 = Array[[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,1.0,1.0]]
 normal = [1.0, 1.0, 1.0]
-println(LineSegment(testpoints[1],testpoints[2],testpoints[3], 0.5, normal))
-println(LineSegment(testpoints[1],testpoints[3],testpoints[2], 0.5, normal))
-println(LineSegment(testpoints[3],testpoints[1],testpoints[2], 0.5, normal))
-println(LineSegment(testpoints[3],testpoints[2],testpoints[1], 0.5, normal))
-println(LineSegment(testpoints[2],testpoints[3],testpoints[1], 0.5, normal))
-println(LineSegment(testpoints[2],testpoints[1],testpoints[3], 0.5, normal))
+l1 = LineSegment(testpoint1[3],testpoint1[1],testpoint1[2], 0.5, normal)
+l2 = LineSegment(testpoint1[3],testpoint1[2],testpoint1[1], 0.5, normal)
+l3 = LineSegment(testpoint1[3],testpoint1[2],testpoint1[1], 0.0, normal)
+
+testpoint2 = Array[[0.0,0.0,0.0],[1.0,1.0,1.0],[0.0,1.0,0.0]]
+l4 = LineSegment(testpoint2[2],testpoint2[1],testpoint2[3], 0.5, normal)
+l5 = LineSegment(testpoint2[2],testpoint2[3],testpoint2[1], 0.5, normal)
+
+Test.with_handler(test_handler) do
+    @test l1.start == [0.0, 0.5]
+    @test l1.finish == [0.0, 1.0]
+    @test l2.start == [0.0, 1.0]
+    @test l2.finish == [0.0, 0.5]
+    @test l3.start == [0.0, 1.0]
+    @test l3.finish == [0.0, 0.0]
+    @test l4.start == [0.5, 0.5]
+    @test l4.finish == [0.5, 1.0]
+    @test l5.start == [0.5, 1.0]
+end
 
 # test binary STL
+println("Testing Binary STL import...")
 binarymesh = PolygonMesh("./data/cube_binary.stl")
-println(binarymesh)
+Test.with_handler(test_handler) do
+    @test binarymesh.bounds == Bounds(10.0,10.0,10.0,0.0,0.0,0.0)
+end
+
+# test mesh rotation
+println("Testing Mesh Rotation...")
+r1 = rotate(1, 1, 0, 0, 0, 0, 1, 0, 0, pi/2)
+r2 = rotate(1, 1, 0, 0, 0, 0, 0, 1, 0, pi/2)
+r3 = rotate(1, 1, 0, 0, 0, 0, 0, 0, 1, pi/2)
+rr1 = [1.0, 0, 1.0]
+rr2 = [0.0, 1.0, -1.0]
+rr3 = [-1.0, 1.0, 0.0]
+Test.with_handler(test_handler) do
+    for i = 1:3
+        @test isapprox(r1[i] ,rr1[i])
+        @test isapprox(r2[i] ,rr2[i])
+        @test isapprox(r3[i] ,rr3[i])
+    end
+end
+
 rotate!(binarymesh, 45.0, [1.0,0.0,0.0], [0.0,0.0,0.0])
-println(binarymesh)
+Test.with_handler(test_handler) do
+    @test binarymesh.bounds == Bounds(10.0,10.0,13.762255133518483,0.0,-8.509035245341185,0.0)
+end
+rotate!(binarymesh, 45.0, [0.0,1.0,0.0], [0.0,0.0,0.0])
+Test.with_handler(test_handler) do
+    @test binarymesh.bounds == Bounds(16.96357128682594,10.0,13.762255133518483,0.0,-8.509035245341185,-8.509035245341185)
+end
