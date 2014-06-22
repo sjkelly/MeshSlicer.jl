62aa839f598d384ed826875db063c61ca6e1ff27
Steve Kelly <kd2cca@gmail.com>
2014-06-22 18:53:45 -0400

    remove test handlers

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,6 @@ using MeshSlicer
 using Base.Test
 using ImmutableArrays
 
-# Since we use relative paths, you need to test from MeshSlicer/test
-
-# setup test handlers
-test_handler(r::Test.Success) = println("Test success: $(r.expr)")
-test_handler(r::Test.Failure) = error("Test fail: $(r.expr)")
-test_handler(r::Test.Error) = rethrow(r)
-
 # getmesh/getface test
 println("Testing PolygonMesh...")
 
@@ -24,21 +17,19 @@ testBounds = Bounds{Float64}(10.0,10.0,10.0,0,0,0)
 
 resultmesh = PolygonMesh("./data/cube.stl")
 
-Test.with_handler(test_handler) do
-    @test resultmesh.faces[end].vertices == testFace.vertices
-    @test resultmesh.faces[end].normal == testFace.normal
-end
+
+@test resultmesh.faces[end].vertices == testFace.vertices
+@test resultmesh.faces[end].normal == testFace.normal
+
 
 println("Testing Bounds...")
-Test.with_handler(test_handler) do
-    @test resultmesh.bounds.xmax == testBounds.xmax
-    @test resultmesh.bounds.ymax == testBounds.ymax
-    @test resultmesh.bounds.zmax == testBounds.zmax
-    @test resultmesh.bounds.xmin == testBounds.xmin
-    @test resultmesh.bounds.ymin == testBounds.ymin
-    @test resultmesh.bounds.zmin == testBounds.zmin
-    @test resultmesh.bounds == testBounds
-end
+@test resultmesh.bounds.xmax == testBounds.xmax
+@test resultmesh.bounds.ymax == testBounds.ymax
+@test resultmesh.bounds.zmax == testBounds.zmax
+@test resultmesh.bounds.xmin == testBounds.xmin
+@test resultmesh.bounds.ymin == testBounds.ymin
+@test resultmesh.bounds.zmin == testBounds.zmin
+@test resultmesh.bounds == testBounds
 
 
 # Test bounds on translated cube
@@ -50,23 +41,22 @@ updateFace = Face(Vector3[[5.0,3.0,4.0],
                       Vector3(-1.0,0.0,0.0)) # contains correct bounds for cube
 update!(testBounds2, updateFace)
 resultmesh2 = PolygonMesh("./data/translated_cube.stl")
-Test.with_handler(test_handler) do
-    @test resultmesh2.bounds.xmax == testBounds2.xmax
-    @test resultmesh2.bounds.ymax == testBounds2.ymax
-    @test resultmesh2.bounds.zmax == testBounds2.zmax
-    @test resultmesh2.bounds.xmin == testBounds2.xmin
-    @test resultmesh2.bounds.ymin == testBounds2.ymin
-    @test resultmesh2.bounds.zmin == testBounds2.zmin
-    @test resultmesh2.bounds == testBounds2
-end
+
+@test resultmesh2.bounds.xmax == testBounds2.xmax
+@test resultmesh2.bounds.ymax == testBounds2.ymax
+@test resultmesh2.bounds.zmax == testBounds2.zmax
+@test resultmesh2.bounds.xmin == testBounds2.xmin
+@test resultmesh2.bounds.ymin == testBounds2.ymin
+@test resultmesh2.bounds.zmin == testBounds2.zmin
+@test resultmesh2.bounds == testBounds2
 
 
 # Test patio detection
 println("Tesing patio detection...")
-Test.with_handler(test_handler) do
-    @test resultmesh2.patios == [3.0, 9.0]
-    @test resultmesh.patios == [0.0, 10.0]
-end
+
+@test resultmesh2.patios == [3.0, 9.0]
+@test resultmesh.patios == [0.0, 10.0]
+
 
 # Test LineSegment
 testpoint1 = Vector3[[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,1.0,1.0]]
@@ -86,43 +76,39 @@ testpoint3 = Vector3[[0.0,0.0,0.0],[0.0,1.0,1.0],[0.0,0.0,2.0]]
 l8 = LineSegment(testpoint3[1], testpoint3[2], testpoint3[3], 0.9, normal)
 l9 = LineSegment(testpoint3[3], testpoint3[1], testpoint3[2], 1.5, normal)
 
-Test.with_handler(test_handler) do
-    @test l1.start == Vector2(0.0, 0.5)
-    @test l1.finish == Vector2(0.0, 1.0)
-    @test l2.start == Vector2(0.0, 1.0)
-    @test l2.finish == Vector2(0.0, 0.5)
-    @test l3.start == Vector2(0.0, 1.0)
-    @test l3.finish == Vector2(0.0, 0.0)
-    @test l4.start == Vector2(0.5, 0.5)
-    @test l4.finish == Vector2(0.5, 1.0)
-    @test l5.start == Vector2(0.5, 1.0)
-    @test l6.start == Vector2(10.0, 0.0)
-    @test l6.finish == Vector2(10.0, 5.0)
-    @test l6.normal == Vector3(1.0,0.0,0.0)
-    @test l7.start == Vector2(1.0, 1.0)
-    @test l7.finish == Vector2(1.0, 1.0)
-    @test l8.start == Vector2(0.0, 0.9)
-    @test l8.finish == Vector2(0.0, 0.0)
-    @test l9.start == Vector2(0.0, 0.0)
-    @test l9.finish == Vector2(0.0, 0.5)
-end
+
+@test l1.start == Vector2(0.0, 0.5)
+@test l1.finish == Vector2(0.0, 1.0)
+@test l2.start == Vector2(0.0, 1.0)
+@test l2.finish == Vector2(0.0, 0.5)
+@test l3.start == Vector2(0.0, 1.0)
+@test l3.finish == Vector2(0.0, 0.0)
+@test l4.start == Vector2(0.5, 0.5)
+@test l4.finish == Vector2(0.5, 1.0)
+@test l5.start == Vector2(0.5, 1.0)
+@test l6.start == Vector2(10.0, 0.0)
+@test l6.finish == Vector2(10.0, 5.0)
+@test l6.normal == Vector3(1.0,0.0,0.0)
+@test l7.start == Vector2(1.0, 1.0)
+@test l7.finish == Vector2(1.0, 1.0)
+@test l8.start == Vector2(0.0, 0.9)
+@test l8.finish == Vector2(0.0, 0.0)
+@test l9.start == Vector2(0.0, 0.0)
+@test l9.finish == Vector2(0.0, 0.5)
+
 
 # test binary STL
 println("Testing Binary STL import...")
 binarymesh = PolygonMesh("./data/cube_binary.stl")
-Test.with_handler(test_handler) do
-    @test binarymesh.bounds == Bounds(10.0,10.0,10.0,0.0,0.0,0.0)
-end
+@test binarymesh.bounds == Bounds(10.0,10.0,10.0,0.0,0.0,0.0)
 
 println("Testing slicing...")
 slice1 = MeshSlice(binarymesh, [0.0,5.0,10.0])
-Test.with_handler(test_handler) do
-    for slice in slice1
-        for poly in slice.polygons
-            n = length(poly.segments)
-            for i = 1:n-1
-                @test poly.segments[i].finish == poly.segments[i+1].start
-            end
+for slice in slice1
+    for poly in slice.polygons
+        n = length(poly.segments)
+        for i = 1:n-1
+            @test poly.segments[i].finish == poly.segments[i+1].start
         end
     end
 end
@@ -130,10 +116,8 @@ end
 println("Tesing Polygon construction...")
 l1 = LineSegment(Vector2(0.0,0.0), Vector2(0.0,1.0), Vector3(0.0,0.0,0.0))
 l2 = LineSegment(Vector2(0.2,1.0), Vector2(2.0,1.0), Vector3(0.0,0.0,0.0))
-Test.with_handler(test_handler) do
-    @test length(Polygon([l1,l2], 0.5)) == 1
-    @test length(Polygon([l1,l2], 0.1)) == 2
-end
+@test length(Polygon([l1,l2], 0.5)) == 1
+@test length(Polygon([l1,l2], 0.1)) == 2
 
 # test mesh rotation
 println("Testing Mesh Rotation...")
@@ -143,19 +127,14 @@ r3 = rotate(1, 1, 0, 0, 0, 0, 0, 0, 1, pi/2)
 rr1 = [1.0, 0, 1.0]
 rr2 = [0.0, 1.0, -1.0]
 rr3 = [-1.0, 1.0, 0.0]
-Test.with_handler(test_handler) do
-    for i = 1:3
-        @test isapprox(r1[i] ,rr1[i])
-        @test isapprox(r2[i] ,rr2[i])
-        @test isapprox(r3[i] ,rr3[i])
-    end
+for i = 1:3
+    @test isapprox(r1[i] ,rr1[i])
+    @test isapprox(r2[i] ,rr2[i])
+    @test isapprox(r3[i] ,rr3[i])
 end
 
 rotate!(binarymesh, 45.0, [1.0,0.0,0.0], [0.0,0.0,0.0])
-Test.with_handler(test_handler) do
-    @test binarymesh.bounds == Bounds(10.0,10.0,13.762255133518483,0.0,-8.509035245341185,0.0)
-end
+@test binarymesh.bounds == Bounds(10.0,10.0,13.762255133518483,0.0,-8.509035245341185,0.0)
+
 rotate!(binarymesh, 45.0, [0.0,1.0,0.0], [0.0,0.0,0.0])
-Test.with_handler(test_handler) do
-    @test binarymesh.bounds == Bounds(16.96357128682594,10.0,13.762255133518483,0.0,-8.509035245341185,-8.509035245341185)
-end
+@test binarymesh.bounds == Bounds(16.96357128682594,10.0,13.762255133518483,0.0,-8.509035245341185,-8.509035245341185)
